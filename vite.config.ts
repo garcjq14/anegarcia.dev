bd6ce593f278be402fd4667d7c742a6ddeb73432
garcjq14 <garcjq14@gmail.com>
2025-04-04 18:35:05 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/portfolio-dynamic-quiz-72-main/",
+export default defineConfig({
+  base: "/",
   publicDir: "public",
   server: {
     host: "::",
     port: 8080,
-    strictPort: true,
-    hmr: {
-      overlay: true
-    },
-    open: true,
-    middlewareMode: false,
+    open: true
   },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -34,4 +23,4 @@ export default defineConfig(({ mode }) => ({
     assetsDir: 'assets',
     sourcemap: true
   }
-}));
+}); 
